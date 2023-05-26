4e72b2e7cc5f935088311bbcf1bea7e326c496a9
ipozuelo <ipozuelo@emeal.nttdata.com>
2023-05-26 15:04:59 +0200

    delete module-federation

--- root-app/webpack.config.js
+++ root-app/webpack.config.js
@@ -10,41 +10,6 @@ module.exports = (webpackConfigEnv, argv) => {
     webpackConfigEnv,
     argv,
     disableHtmlGeneration: true,
-    output: {
-      publicPath: "http://localhost:4001/",
-    },
-  
-    resolve: {
-      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-    },
-  
-    devServer: {
-      port: 4001,
-      historyApiFallback: true,
-    },
-  
-    module: {
-      rules: [
-        {
-          test: /\.m?js/,
-          type: "javascript/auto",
-          resolve: {
-            fullySpecified: false,
-          },
-        },
-        {
-          test: /\.(css|s[ac]ss)$/i,
-          use: ["style-loader", "css-loader", "postcss-loader"],
-        },
-        {
-          test: /\.(ts|tsx|js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-      ],
-    }
   });
 
   return merge(defaultConfig, {
@@ -57,28 +22,6 @@ module.exports = (webpackConfigEnv, argv) => {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
-      }),
-      new ModuleFederationPlugin({
-        name: "host_app",
-        filename: "remoteEntry.js",
-        remotes: {
-          components: "remote_app@http://localhost:4002/remoteEntry.js",
-        },
-        exposes: {},
-        shared: {
-          ...deps,
-          react: {
-            singleton: true,
-            requiredVersion: deps.react,
-          },
-          "react-dom": {
-            singleton: true,
-            requiredVersion: deps["react-dom"],
-          },
-        },
-      }),
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
       }),
     ],
   });
